483cdc271f9a658cd4a7f2f771667d3260d4d762
Guilherme Nunes <Guilherme.Nunes@AB-inbev.com>
2022-09-10 01:56:26 -0300

    First commit :tada:

---
+++ next.config.js
@@ -0,0 +1,20 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  swcMinify: true,
+}
+module.exports = {
+  // Outras configurações do Nextjs
+  webpack: (config, options) => {
+    // Configura o @svgr como um loader para imagens SVG usando o hook para o Webpack
+    config.module.rules.push({
+      test: /\.svg$/,
+      use: ["@svgr/webpack"],
+    });
+
+    // O objeto config modificado precisa ser retornado
+    return config;
+  },
+};
+
+module.exports = nextConfig
